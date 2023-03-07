232e63b8cdafbc69c174c31184100550afd8d6f4
sabrinajm <126985573+sabrinajm@users.noreply.github.com>
2023-03-07 19:59:38 +0000

    Tue, Mar 7, 2023, 7:59 PM +00:00

--- hello.c
+++ hello.c
@@ -1,8 +1,6 @@
-#include<cs50.h>
 #include<stdio.h>
 int main(void)
 {
-    string first = get_string("what is your first name?");
-    string last = get_string("what is your last name?");
+
     printf("%%\n");
 }
