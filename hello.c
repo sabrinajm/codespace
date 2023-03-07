26a49912f9a703922d464d902b66b2b5df296579
sabrinajm <126985573+sabrinajm@users.noreply.github.com>
2023-03-07 07:13:18 +0000

    Tue, Mar 7, 2023, 7:13 AM +00:00

--- hello.c
+++ hello.c
@@ -4,5 +4,5 @@ int main(void)
 {
     string first = get_string("what is your first name?");
     string last = get_string("what is your last name?");
-    printf("hello, %s %s!\n", first, last);
+    printf("I got 100%%\n");
 }
