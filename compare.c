f3a41a4808443735e371123ebf4b6a0f1b2cdf8b
sabrinajm <126985573+sabrinajm@users.noreply.github.com>
2023-03-07 07:32:23 +0000

    Tue, Mar 7, 2023, 7:32 AM +00:00

--- compare.c
+++ compare.c
@@ -1,2 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("what is x?");
+    int y = get_int("what is y?"):
+
+    if (x<y)
+{
+    printf("x is less than y\n");
+}
