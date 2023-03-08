b190d15326b018bb9aca0d8b36e606459c02171e
sabrinajm <126985573+sabrinajm@users.noreply.github.com>
2023-03-08 12:40:41 +0000

    Wed, Mar 8, 2023, 12:40 PM +00:00

--- mario.c
+++ mario.c
@@ -4,8 +4,9 @@ int main(void)
 {
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
         {
             printf("#");
         }
+}
 }
